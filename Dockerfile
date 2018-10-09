bd4a9eae14e1531672d062fa23fd917c407e23a9
Xiaoquan Kong <u1mail2me@gmail.com>
2018-10-09 21:50:13 +0800

    Update

--- Dockerfile
+++ Dockerfile
@@ -8,4 +8,7 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 EXPOSE 5002
 
+ENV ACTION_ENDPOINT_URL "http://0.0.0.0:5055/webhook"
+ENV NLU_URL "http://127.0.0.1:5000"
+
 CMD [ "./run_server.bash" ]
--- run_server.bash
+++ run_server.bash
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+python ./render.py
+
 python -m rasa_core.run -d models/dialogue -u default/current \
        --port 5002 --credentials credentials.yml \
        --cors * --endpoints endpoints.yml
