eeffb621375072c842f79ae8e834e414ddac0683
Xiaoquan Kong <u1mail2me@gmail.com>
2018-11-01 01:40:20 +0800

    Using fallback policy

--- train_CORE.bash
+++ train_CORE.bash
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-python -m rasa_core.train -s stories.md -d domain.yml -o models/dialogue --epochs 500 --endpoints endpoints.yml
+python -m rasa_core.train -s stories.md -d domain.yml -o models/dialogue --epochs 500 --nlu_threshold 0.4  --core_threshold 0.4
